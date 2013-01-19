8cd7152f37f36a0dd2d2d6a4c7e6d849102f49c4
Patrick Jameson <patrickejameson@gmail.com>
2013-01-19 00:04:43 -0500

    OMG

---
+++ Placear/src/com/snowneedle/placear/Placear.java
@@ -0,0 +1,18 @@
+package com.snowneedle.placear;
+
+import com.snowneedle.placear.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class Placear extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_placear);
+		PlacearCamera preview = new PlacearCamera(this);
+		((FrameLayout) findViewById(R.id.preview)).addView(preview);
+	}
+}
