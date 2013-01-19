efe356bfce85fb386b64c13d693c36328a0e116a
jpeacockinc <jpeacockinc@Gmail.com>
2013-01-19 07:23:03 -0500

    Title Bar Removed

--- Placear/src/com/snowneedle/placear/Placear.java
+++ Placear/src/com/snowneedle/placear/Placear.java
@@ -4,6 +4,8 @@ import com.snowneedle.placear.R;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 public class Placear extends Activity {
@@ -13,6 +15,10 @@ public class Placear extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
 		setContentView(R.layout.activity_placear);
 		PlacearCamera preview = new PlacearCamera(this);
