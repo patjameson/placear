607c445b75a317b5e6d3a599afbfc03295ab0a6a
Patrick Jameson <patrickejameson@gmail.com>
2013-01-19 21:59:47 -0500

    DEBUG

--- Placear/src/com/snowneedle/placear/Placear.java
+++ Placear/src/com/snowneedle/placear/Placear.java
@@ -4,26 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
-
-import com.snowneedle.placear.Place;
 import com.snowneedle.placear.R;
 import com.snowneedle.placear.location.PlacearLocationManager;
 
 import android.app.Activity;
-import android.opengl.GLSurfaceView;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.LocationManager;
-import android.util.Log;
 
 import android.widget.FrameLayout;
 
@@ -38,13 +26,12 @@ public class Placear extends Activity {
 		
 		setContentView(R.layout.activity_placear);
 		
-		
 		sensorManager = (SensorManager)this.getSystemService(Context.SENSOR_SERVICE);
 		
 		LocationManager lm = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-		PLM = new PlacearLocationManager(lm, sensorManager, getGoogleAccessToken(), this);
+		PLM = new PlacearLocationManager(lm, sensorManager, getGoogleAccessToken());
 		
-		preview = new PlacearCamera(this, PLM.locationListener);
+		preview = new PlacearCamera(this, PLM);
 		FrameLayout f = ((FrameLayout) findViewById(R.id.preview));
 		f.addView(preview);
 	}
@@ -60,10 +47,6 @@ public class Placear extends Activity {
 			e.printStackTrace();
 		}
 		
-		Log.w("access token", token);
-		
 		return token;
 	}
-	
-	
 }
--- Placear/src/com/snowneedle/placear/PlacearCamera.java
+++ Placear/src/com/snowneedle/placear/PlacearCamera.java
@@ -1,97 +1,75 @@
 package com.snowneedle.placear;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
-import com.snowneedle.placear.API.PlaceWorker;
-import com.snowneedle.placear.location.PlacearLocationListener;
-import com.snowneedle.placear.location.PlacearLocationListener.Refresh;
 import com.snowneedle.placear.location.PlacearLocationManager;
 
-import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.hardware.Camera;
-import android.location.Location;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class PlacearCamera extends SurfaceView implements SurfaceHolder.Callback, Observer {
-	SurfaceHolder mHolder;
-	public Camera camera;
-	Paint paint = new Paint();
-	PlacearLocationListener ll;
-	String text = "Nothing directly ahead...fd";
+public class PlacearCamera extends SurfaceView implements SurfaceHolder.Callback {
+	private SurfaceHolder mHolder;
+	private Camera camera;
+	private Paint paint;;
+	private PlacearLocationManager plm;
 
-	PlacearCamera(Placear context, PlacearLocationListener cam) {
+	PlacearCamera(Placear context, PlacearLocationManager _plm) {
 		super(context);
 		this.setWillNotDraw(false);
 		
-		ll = cam;
+		paint = new Paint();
 		
-//		Refresh worker = cam.refreshForLocation(this);
-//		new Thread(worker).start();
-//		worker.addObserver(this);
+		plm = _plm;
 
 		mHolder = getHolder();
 		mHolder.addCallback(this);
 		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 	}
 	
-	@Override
-	public void update(Observable observable, Object data) {
-		//this.invalidate();
-	}
-	
 	@Override 
     public void onDraw(Canvas canvas) {
 		paint.setColor(Color.RED);
 		paint.setStrokeWidth(3);
-		paint.setTextSize(40);
-//		canvas.translate(20, 20);
-//		canvas.rotate(20);
-//		canvas.translate(-10, -10);
+		paint.setTextSize(35);
+		canvas.rotate(-90, 250, 250);
 		
-//		android.graphics.Camera cam = new android.graphics.Camera();
-//		cam.save();
-//		ca
-		
-//		Matrix imageMatrix = new Matrix();
-//	    float[] srcPoints = {
-//	        0, 0,
-//	        0, 200,
-//	        200, 200,
-//	        200, 0};
-//	    int rotation = -150;
-//	    float[] destPoints = {
-//	        rotation, rotation/2f,
-//	        rotation, 200 - rotation/2f,
-//	        200 - rotation, 200,
-//	        200 - rotation, 0};
-//	    imageMatrix.setPolyToPoly(srcPoints, 0, destPoints, 0, 4);
-//	    canvas.setMatrix(imageMatrix);
-//	    canvas.rotate(10);
-		if (ll != null) {
-			Place loc = ll.getClosestLocation();
-			if (loc != null)
-				canvas.drawText(loc.getName(), 300, 200, paint);
+		if (plm != null) {
+			Place loc = plm.getClosestLocation();
+			if (loc != null) {
+				canvas.drawText(loc.getName(), 100, 50, paint);
+				canvas.drawText("Bearing: " + plm.getAzimuth(), 100, 100, paint);
+				canvas.drawText("Acc: " + plm.getCurrentLocation().getAccuracy(), 100, 150, paint);
+				canvas.drawText("lat: " + plm.getCurrentLocation().getLatitude(), 100, 200, paint);
+				canvas.drawText("long: " + plm.getCurrentLocation().getLongitude(), 100, 250, paint);
+				canvas.drawText("bearto: " + plm.getCurrentLocation().bearingTo(loc.getLocation()), 100, 300, paint);
+				
+				float direction = plm.getAzimuth();
+				
+				if (direction > 180)
+					direction = -(360 - direction);
+				canvas.drawText("bearlive: " + Math.abs(plm.getCurrentLocation().bearingTo(loc.getLocation())-direction), 100, 350, paint);
+			}
+		} else {
+			canvas.drawText("waiting...", 300, 200, paint);
 		}
+		
 		try {
 			Thread.currentThread().sleep(200);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
 		this.invalidate();
     }
 	
 
 	public void surfaceCreated(SurfaceHolder holder) {
-		// The Surface has been created, acquire the camera and tell it where
-		// to draw.
 		camera = Camera.open(0);
 		try {
 			camera.setPreviewDisplay(holder);
